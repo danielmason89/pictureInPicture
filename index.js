87f14451b6af26195d76d545bab9b352a1ab1584
Daniel Mason <danmasson0@gmail.com>
2022-06-13 11:58:23 -0400

    implement some html, relevant CSS styling, and a bit of JavaScript to create a little picture in picture app, to help me with concentration.

--- index.js
+++ index.js
@@ -1,62 +1,29 @@
-const quoteContainer = document.getElementById("quote-container");
-const quoteText = document.getElementById("quote");
-const authorText = document.getElementById("author");
-const twitterBtn = document.getElementById("twitter");
-const newQuoteBtn = document.getElementById("new-quote");
-const loader = document.getElementById("loader");
-let apiQuotes = [];
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
 
-function showLoadingSpinner() {
-  loader.hidden = false;
-  quoteContainer.hidden = true;
-}
-function removeLoadingSpinner() {
-  quoteContainer.hidden = false;
-  loader.hidden = true;
-}
+// Prompt the user to select media stream, pass video element , then play the video
 
-// Show new Quote
-function newQuote() {
-  showLoadingSpinner();
-  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  // Check if Author field is blacnk and replace with unknown
-  if (!quote.author) {
-    authorText.textContent = "Phuong Nguyen";
-  } else {
-    authorText.textContent = quote.author;
-  }
-  // Check Quote length to determine styling
-  if (quote.text.length > 100) {
-    quoteText.classList.add("long-quote");
-  } else {
-    quoteText.classList.remove("long-quote");
-  }
-  // Set the quote, Hide the loader\
-  quoteText.textContent = quote.text;
-  removeLoadingSpinner();
-}
-
-// Get Quotes from Api
-async function getQuotes() {
-  showLoadingSpinner();
-  const apiURL = "https://type.fit/api/quotes";
+async function getMediaStream() {
   try {
-    const response = await fetch(apiURL);
-    apiQuotes = await response.json();
-    newQuote();
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    }
   } catch (error) {
-    getQuotes();
-  }
-}
+    // Handle error
+    console.log("Whoops, error here:", error);
+  }}
 
-// Tweet a Quote
-function tweetQuote() {
-  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
-  window.open(twitterUrl, "_blank");
-}
+  button.addEventListener('click', async () => {
+    // Disable the button so the user can't click it twice
+    button.disabled = true;
+    // Start Picture in Picture
+    await videoElement.requestPictureInPicture();
+    // Re-enable the button
+    button.disabled = false;
 
-// Event listener
-newQuoteBtn.addEventListener("click", newQuote);
-twitterBtn.addEventListener("click", tweetQuote);
+  });
 
-getQuotes();
+  // on Load
+  getMediaStream();
